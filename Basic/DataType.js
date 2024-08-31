cd20d83baa03001a4112534a56885d3250fad1ab
kirshemca28 <kirtishekhar6255@gmail.com>
2024-08-31 15:13:55 +0000

    JS

---
+++ Basic/DataType.js
@@ -0,0 +1,29 @@
+"Use strict"; // treat all js code as newer version
+
+// alert('Hello') // We are using nodejs, not browser
+// code readability should be high
+
+let name = "Name" //string datatype
+let age = 28 // Number
+let isLoggedIn = false // Boolean
+
+console.log("Kirti");
+
+
+// Premitive datatypes
+//number => 2 to power 53 (range memory address)
+// bigint
+// string => ""
+// boolean = true or false
+// *null => standalone value (empty value) (let state = null) .... Object
+// undefined =>  let state; undefined, no state name given
+// symbol => (Unique, On React) 
+
+// object
+
+console.log(typeof "Kirti");
+
+console.log(typeof age);
+
+console.log(typeof null);
+console.log(typeof undefined);
