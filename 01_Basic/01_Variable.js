ce88b29cc49d4d3bf1c4c3ba318b02869b5cb40f
kirshemca28 <kirtishekhar6255@gmail.com>
2024-08-31 14:32:14 +0000

    Variable

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,20 @@
+const accountId = 123456
+let accountEmail = "info@gmail.com"
+var accountPassword = "007700"
+accountCity = "Patna"
+let accountState = "Bihar"
+
+//accountId = 2 // not allowed
+
+/*
+Prefer not to use var
+Because of issues in block scope and functional scope
+*/
+accountEmail = "email@gmail.com"
+accountPassword = "458458"
+accountCity = "Gaya"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
