9618f5334d99550164eb44f835ed5e73e5ed392d
kirshemca28 <kirtishekhar6255@gmail.com>
2024-08-31 16:03:54 +0000

    JS

---
+++ Basic/Comparison.js
@@ -0,0 +1,15 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined > 0);
+
+// Not to use above code
+
+// === strctly check
+
+console.log("2" === 2);
+
+
