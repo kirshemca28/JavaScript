924bc30bcfae3b1d16233171663ec61dede0c48b
kirshemca28 <kirtishekhar6255@gmail.com>
2024-09-01 06:34:38 +0000

    JS

--- Basic/String.js
+++ Basic/String.js
@@ -5,7 +5,7 @@ const repoCount = 1
 
 console.log(`Hello my name is ${name} and my repCount is ${repoCount}`);
 
-const gameName = new String('kirtihc')
+const gameName = new String('kirti-hc')
 console.log(gameName[0]);
 
 
@@ -13,3 +13,23 @@ console.log(gameName[0]);
 
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 3)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   Kirti   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://wwww.google.com"
+console.log(url.replace('%20','-'));
+console.log(url.includes('google')); //google exist so --true
+console.log(url.includes('kirti')); //kirti not exist so --false
+
+
+console.log(gameName.split('-'));
