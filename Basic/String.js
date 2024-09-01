87f1966540b6c99526c8ac56ce304254d95f462f
kirshemca28 <kirtishekhar6255@gmail.com>
2024-09-01 03:22:59 +0000

    JS

---
+++ Basic/String.js
@@ -0,0 +1,15 @@
+const name = "Kirti"
+const repoCount = 1
+
+//console.log(name + repCount + "Value");
+
+console.log(`Hello my name is ${name} and my repCount is ${repoCount}`);
+
+const gameName = new String('kirtihc')
+console.log(gameName[0]);
+
+
+//console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
